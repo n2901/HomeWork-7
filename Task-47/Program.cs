24f1191c9b6b0d11944d05b5c64ff8be8fdcaee6
nadezhda <kissulia83@mail.ru>
2023-06-04 13:36:28 +0300

    add Task-47

---
+++ Task-47/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round((new Random().NextDouble() * 10), 1);
+        }
+    }
+}
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int row = ReadInt("Введите число M: ");
+int colums = ReadInt("Введите число N: ");
+double[,] numbers = new double[row, colums];
+FillArray2D(numbers);
+PrintArray2D(numbers);
